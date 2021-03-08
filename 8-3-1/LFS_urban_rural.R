fe9f530faae9177026584612c1c05ddd65e4d272
Wood <emma.wood@ons.gov.uk>
2021-03-08 16:28:28 +0000

    edits during meeting with Varun
    Signed-off-by: Wood <emma.wood@ons.gov.uk>

--- 8-3-1/LFS_urban_rural.R
+++ 8-3-1/LFS_urban_rural.R
@@ -16,29 +16,43 @@ lfs_data <- read_sav("X:\\LFS Person datasets\\2018 reweighted data\\2020\\LFSP_
 # SEX: 1 = M, 2 = F
 
 # questions: does OWNBUS and RELBUS always refer to main job? ans = yes
-#            is the sample size for 'agriculture large enough?
+#            is the sample size for 'agriculture' large enough? Or do we need to use 'Agriculture, foresty and fishing'
 #
 #            Can we recreate the headline figures? Can use this to answer:
 #            can we use pre-2018 data using the old weights and present them alongside the 2018 onwards data?
-#            which quarter are the headline figures from? We should use the same dataset for this analysus.
+#            which quarter are the headline figures from? Or is there an annual dataset? We should use the same dataset for this analysus.
 
 
 unpaid_family_workers_OWNBUS_RELBUS <- lfs_data %>% 
-  select(OWNBUS, RELBUS, STAT, STATR, ICDM, INDS07M, SEX, PWT18) %>% 
-  filter(OWNBUS == 1 | RELBUS == 1)
+  select(OWNBUS, RELBUS, STAT, STATR, ICDM, INDS07M, SEX, PWT18, CASENO) %>% 
+  filter(OWNBUS == 1 | RELBUS == 1) %>% 
+  mutate(OWNBUS_RELBUS = TRUE)
 
 unpaid_family_workers_STAT_STATR <- lfs_data %>% 
-  select(OWNBUS, RELBUS, STAT, STATR, ICDM, INDS07M, SEX, PWT18) %>% 
-  filter(STAT == 4 | STATR == 4)
+  select(OWNBUS, RELBUS, STAT, STATR, ICDM, INDS07M, SEX, PWT18, CASENO) %>% 
+  filter(STAT == 4 | STATR == 4) %>% 
+  mutate(STAT_STATR = TRUE) 
 
-# unpaid_family_worker_not_STAT4 <- unpaid_family_workers_OWNBUS_RELBUS %>% 
-#   filter(STAT != 4)
-# unpaid_family_worker_not_STATR4 <- unpaid_family_workers_OWNBUS_RELBUS %>% 
-#   filter(STATR != 4)
-# unpaid_family_worker_not_OWNBUS_RELBUS <- unpaid_family_workers_STAT_STATR %>% 
-#   filter(OWNBUS != 1 & RELBUS != 1)
-# # looks like STATR 4 and STAT 4 directly map to OWNBUS/RELBUS
+find_repeats_STAT_STATR <- unpaid_family_workers_STAT_STATR %>% 
+  group_by(CASENO) %>% 
+  summarise(count = n()) %>% 
+  mutate(STAT_STATR = TRUE)
+find_repeats_OWNBUS_RELBUS <- unpaid_family_workers_OWNBUS_RELBUS %>% 
+  group_by(CASENO) %>% 
+  summarise(count = n()) %>% 
+  mutate(OWNBUS_RELBUS = TRUE)
+
+mismatched_rows <- find_repeats_STAT_STATR %>% 
+  left_join(find_repeats_OWNBUS_RELBUS, by = "CASENO") %>% 
+  mutate(OWNBUS_RELBUS = ifelse(is.na(OWNBUS_RELBUS), FALSE, OWNBUS_RELBUS)) %>% 
+  filter(OWNBUS_RELBUS == FALSE) %>% 
+  distinct(CASENO)
 
 # WHY IS there is an extra 2 rows in unpaid_family_workers_STAT_STATR compared to unpaid_family_workers_OWNBUS_RELBUS???
+extra_rows_from_STAT_STATR <- mismatched_rows %>% 
+  left_join(unpaid_family_workers_STAT_STATR)
+# non-response for OWNBUS and RELBUS - could increase sample size by two by using this approach, but the user guide says to define by OWNBUS and RELBUS
 
-
+unpaid_family_workers <- data.frame(lapply(unpaid_family_workers_OWNBUS_RELBUS, as.character), stringsAsFactors=FALSE) %>% 
+  mutate(PWT18 = as.numeric(PWT18))
+str(unpaid_family_workers)  
