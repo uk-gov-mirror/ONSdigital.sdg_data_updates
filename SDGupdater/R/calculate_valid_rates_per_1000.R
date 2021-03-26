6895ef36093e460b93db420a3d11cf4af64f1258
Wood <emma.wood@ons.gov.uk>
2021-03-26 11:57:49 +0000

    fixed bug in calculate_valid_rates_per_1000
    if statements were buggy. Now fixed
    
    Signed-off-by: Wood <emma.wood@ons.gov.uk>

--- SDGupdater/R/calculate_valid_rates_per_1000.R
+++ SDGupdater/R/calculate_valid_rates_per_1000.R
@@ -19,11 +19,11 @@ calculate_valid_rates_per_1000 <- function (numerator, denominator, decimal_plac
   if (is.numeric(denominator) == FALSE) { stop("denominator must be numeric") }
   if (is.numeric(decimal_places) == FALSE) { stop("decimal_places must be numeric") }
 
-  if (numerator < 0) { stop("numerator must be greater than 0") }
-  if (denominator < 0) { stop("denominator must be greater than 0") }
-  if (decimal_places < 0) { stop("decimal_places must be greater than 0") }
+  if (any(numerator < 0)) { stop("numerator must be greater than 0") }
+  if (any(denominator < 0)) { stop("denominator must be greater than 0") }
+  if (decimal_places < 0) { stop("decimal_places must be greater than or equal to 0") }
 
-  if (numerator > denominator) { stop("numerator must be smaller than or equal to denominator") }
+  if (any(numerator > denominator)) { stop("numerator must be smaller than or equal to denominator") }
 
   ifelse(numerator > 3,
          round(numerator / (denominator / 1000), decimal_places),
